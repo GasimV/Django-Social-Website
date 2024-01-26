38c2997c1a873090b2e05a0a54c14204e48e666e
Gasym Valiyev <outnesss@yandex.ru>
2024-01-26 14:41:19 +0400

    Modified settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -136,4 +136,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
 ]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '' # Facebook App Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+
